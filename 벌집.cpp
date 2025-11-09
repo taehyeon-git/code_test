f4ab917206e95b09b50b04404e989e23edeaba11
taehyeon-git <t.j9431@gmail.com>
2025-11-09 19:09:12 +0900

    백준 2292

---
+++ 벌집.cpp
@@ -0,0 +1,23 @@
+/*
+
+#include <stdio.h>
+
+int main() 
+{
+	int N = 0;
+	int sum = 1;
+	int depth = 1;
+
+	scanf_s("%d", &N);
+
+	while (sum < N)
+	{
+		sum = sum + (depth * 6);
+		depth++;
+	}
+
+	printf("%d", depth);
+
+	return 0;
+}
+*/
