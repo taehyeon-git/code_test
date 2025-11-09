17eaab3804a4e6942c7af2fbc340050decb51086
taehyeon-git <t.j9431@gmail.com>
2025-11-09 19:08:39 +0900

    백준 2720

---
+++ 세탁소.cpp
@@ -0,0 +1,24 @@
+/*
+#include <stdio.h>
+
+int main() {
+    int T, C;
+    scanf_s("%d", &T);
+
+    for (int i = 0; i < T; i++) {
+        scanf_s("%d", &C);
+
+        int q = C / 25;
+        C %= 25;
+        int d = C / 10;
+        C %= 10;
+        int n = C / 5;
+        C %= 5;
+        int p = C;
+
+        printf("%d %d %d %d\n", q, d, n, p);
+    }
+
+    return 0;
+}
+*/
