5bd15f2ab32a2edabf60059b7c7d107ddd5b6583
taehyeon-git <t.j9431@gmail.com>
2025-12-10 17:01:27 +0900

    백준 24313

---
+++ 시간복잡도 이해.cpp
@@ -0,0 +1,20 @@
+/*
+#include <stdio.h>
+
+int main() {
+	int a1, a0;
+	int c;
+	int n0;
+
+	scanf_s("%d %d", &a1, &a0);
+	scanf_s("%d", &c);
+	scanf_s("%d", &n0);
+
+	if (a1 <= c && a1 * n0 + a0 <= c * n0)
+		printf("1");
+	else
+		printf("0");
+
+	return 0;
+}
+*/
